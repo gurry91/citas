8842f794b70ef8d39a1d24dbebe75e64dafc535e
Salvory <salvoryrm@gmail.com>
2017-12-08 10:13:37 +0100

    tristeintentodepasswd

--- php/modificarpassword.php
+++ php/modificarpassword.php
@@ -11,11 +11,26 @@ if(!empty($_POST)){
 	$consulta=mysqli_query($conexion,$sql)or die(mysqli_error());
 	if($fila=mysqli_fetch_array($consulta,MYSQL_ASSOC)){		
 		if ($fila['password']==$vieja){
-			mysqli_query($conexion,"UPDATE usuarios set password='$nueva' where n_usuario='$usuario'");
-			echo "Contraseña actualizada";
+			mysqli_query($conexion,"UPDATE usuarios set password='$nueva' where n_usuario='$usuario'"); 
+			echo "<script> alert('Contrase\u00f1a actualizada') </script>";	
+				
+			header( "Refresh:2; url=http://192.168.56.2/citas/php/cerrarsesion.php", true, 303);
+
+			
+			
 		}else{
-			echo "La contraseña introducida no es correcta";
+		//	echo "<script> alert('Contrase\u00f1a incorrecta') </script>";
+		//	error_log($_SERVER['HTTP_REFERER']);
+			echo "<script> alert($_SERVER['HTTP_REFERER']) </script>";	
+				
+			header( "Refresh:15; url=http://192.168.56.2/citas/frmprofesor.php", true, 303);
+			
+
+				
+			
 		}
+		
+		
 	}
 }
 
